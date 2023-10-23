d5ef4b76c042280d65c8da3e5c0c9137f2b7c63e
SparkerXof <sparket15@gmail.com>
2023-10-23 10:09:08 +0300

    Resources and python gitignore

--- telegram.py
+++ telegram.py
@@ -1,7 +1,8 @@
 import asyncio
 import logging
 import sys
-from os import getenv
+import json
+from os import getenv, listdir
 
 from aiogram import Bot, Dispatcher, Router, types, F
 from aiogram.enums import ParseMode
@@ -10,7 +11,9 @@ from aiogram.types import Message, FSInputFile
 from aiogram.utils.keyboard import InlineKeyboardBuilder
 
 users_sending = [] # Users sending promts store here (Could be changed for security)
-TOKEN = open("./resources/teletoken", "r").read() # Bot's token in file
+user_languages = {}
+languages = {}
+TOKEN = open("resources/teletoken", "r").read() # Bot's token in file
 bot = Bot(TOKEN, parse_mode=ParseMode.HTML)
 
 # Router replaces Dispatcher
@@ -18,59 +21,92 @@ router = Router()
 dp = Dispatcher()
 dp.include_router(router=router)
 
-def get_result_by_promt(promt: str):
+def get_json_string(key: str, user_id: int):
+    try:
+        return languages[user_languages[user_id]][key]
+    except:
+        return languages["en"][key]
+
+def get_result_by_propmt(prompt: str):
     result = "placeholder"
     return result
 
 # Sends start message
 @router.message(CommandStart())
 async def command_start_handler(message: Message) -> None:
+    user_languages[message.from_user.id] = message.from_user.language_code
     builder = InlineKeyboardBuilder()
-    builder.button(text=f"Continue", callback_data=f"to_main") # Go to main message
-    await message.answer(text=f"This is AI Prompt Analyzer project. Click the button to continue.", reply_markup=builder.as_markup())
+    builder.button(text=get_json_string("continue", message.from_user.id), callback_data=f"to_main") # Go to main message
+    await message.answer(text=get_json_string("welcome", message.from_user.id), reply_markup=builder.as_markup())
 
 # Bot menu with Analyzer and Credits buttons
 @router.callback_query(F.data == "to_main")
 async def menu(callback_query: types.CallbackQuery) -> None:
-    if callback_query.from_user.id in users_sending:
-        users_sending.remove(callback_query.from_user.id) # Remove user from array
     title_img = FSInputFile('resources/title_icon.png')
     builder = InlineKeyboardBuilder()
-    builder.button(text=f"Analyze Prompt", callback_data=f"analyze")
-    builder.button(text=f"Credits", callback_data=f"to_credits")
-    text = f"The AI Prompt Analyzer helps people to make good prompts for other AIs. Press \'Analyze Prompt\' for advices."
-    await bot.send_photo(chat_id=callback_query.from_user.id, photo=title_img, caption=text, reply_markup=builder.as_markup())
+    builder.button(text=get_json_string("analyze", callback_query.from_user.id), callback_data=f"analyze")
+    builder.button(text=get_json_string("languages", callback_query.from_user.id), callback_data=f"to_languages")
+    builder.button(text=get_json_string("credits", callback_query.from_user.id), callback_data=f"to_credits")
+    await bot.send_photo(chat_id=callback_query.from_user.id, photo=title_img, caption=get_json_string("menu", callback_query.from_user.id), reply_markup=builder.as_markup())
+    await callback_query.message.delete()
 
 # Bot credits. !!!TODO!!!
 @router.callback_query(F.data == "to_credits")
 async def credits(callback_query: types.CallbackQuery) -> None:
     builder = InlineKeyboardBuilder()
-    builder.button(text=f"Back to menu", callback_data=f"to_main")
-    text = f"Project by:\nMTUCI BVT2206 Group\n\nGitHub repo: TODO"
-    await bot.send_message(chat_id=callback_query.from_user.id, text=text, reply_markup=builder.as_markup())
+    builder.button(text=get_json_string("back", callback_query.from_user.id), callback_data=f"quit")
+    await bot.send_message(chat_id=callback_query.from_user.id, text=get_json_string("info", callback_query.from_user.id), reply_markup=builder.as_markup())
+    await callback_query.answer()
+
+@router.callback_query(F.data == "to_languages")
+async def language_menu(callback_query: types.CallbackQuery) -> None:
+    builder = InlineKeyboardBuilder()
+    for key in list(languages.keys()):
+        builder.button(text=languages[key]["lang"], callback_data=("lang." + key))
+    builder.button(text=get_json_string("back", callback_query.from_user.id), callback_data=f"to_main")
+    await bot.send_message(chat_id=callback_query.from_user.id, text=get_json_string("language_menu", callback_query.from_user.id), reply_markup=builder.as_markup())
+    await callback_query.answer()
+
+@router.callback_query(F.data.contains("lang."))
+async def change_language(callback_query: types.CallbackQuery) -> None:
+    user_languages[callback_query.from_user.id] = callback_query.data.removeprefix("lang.")
+    print(callback_query.data.removesuffix("lang."))
+    await callback_query.answer(get_json_string("language_changed", callback_query.from_user.id))
+
+@router.callback_query(F.data == "quit")
+async def remove_menu(callback_query: types.CallbackQuery) -> None:
+    if callback_query.from_user.id in users_sending:
+        users_sending.remove(callback_query.from_user.id) # Remove user from array
+    await callback_query.message.delete()
 
 # Analyzer. Waits for user's promt
 @router.callback_query(F.data == "analyze")
 async def analyzer_wait(callback_query: types.CallbackQuery):
     builder = InlineKeyboardBuilder()
-    builder.button(text=f"Back to menu", callback_data=f"to_main")
+    builder.button(text=get_json_string("back", callback_query.from_user.id), callback_data=f"quit")
     if callback_query.from_user.id in users_sending:
-        text = f"You've already started analyzer. Press \"Back to menu\" to quit." # Exeption, when user's ID is in array
+        text = get_json_string("analyze_ready", callback_query.from_user.id) # Exeption, when user's ID is in array
     else:
         users_sending.append(callback_query.from_user.id) # User ID added in users_sending array
-        text = f"Write your prompts as message. Press \"Back to menu\" to quit."
+        text = get_json_string("write_prompt", callback_query.from_user.id)
     await bot.send_message(chat_id=callback_query.from_user.id, text=text, reply_markup=builder.as_markup())
+    await callback_query.answer()
 
 # Sends promts to AI.
 @router.message()
-async def promt_to_analyze(message: Message):
+async def prompt_to_analyze(message: Message):
     if message.from_user.id in users_sending: # Works if user is in array
         users_sending.remove(message.from_user.id)
-        await message.answer(text=get_result_by_promt(promt=message.text)) # TODO
+        await message.answer(text=get_json_string("waiting", message.from_user.id)) # TODO
+        await message.answer(text=get_result_by_propmt(prompt=message.text)) # TODO
 
 async def main() -> None:
     await dp.start_polling(bot)
 
 if __name__ == "__main__":
-    logging.basicConfig(level=logging.INFO, stream=sys.stdout)
+    for file in listdir("resources/languages"):
+        if file.endswith(".json"):
+            languages[file.removesuffix(".json")] = json.load(open("resources/languages/" + file))
+            print(file.removesuffix(".json"))
+            logging.basicConfig(level=logging.INFO, stream=sys.stdout)
     asyncio.run(main())
