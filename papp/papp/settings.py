3d75c7bf875a970edbf3d5fd22c5fc20db8d70a9
Lapisu <dmitriiycmo@gmail.com>
2023-11-19 19:35:39 +0300

    fixed authtoken issue

--- papp/papp/settings.py
+++ papp/papp/settings.py
@@ -47,7 +47,6 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework.authtoken',
     'rest_framework',
-    'rest_framework.authtoken',
     'corsheaders',
     'djoser',
     'tra',
