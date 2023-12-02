c8ff1c0ea23ef2ce5a854a48574aaa2b50573316
Lapisu <dmitriiycmo@gmail.com>
2023-12-02 17:45:30 +0300

    tests

--- papp/tra/validations.py
+++ papp/tra/validations.py
@@ -1,12 +1,13 @@
 from django.core.exceptions import ValidationError
 from django.contrib.auth import get_user_model
+from django.contrib.auth.hashers import make_password
 
 UserModel = get_user_model()
 
 def custom_validation(data):
     email = data['email'].strip()
     username = data['username'].strip()
-    password = data['password'].strip()
+    password = make_password(data['password'].strip())
     ##
     if not email or UserModel.objects.filter(email=email).exists():
         raise ValidationError('choose another email')
